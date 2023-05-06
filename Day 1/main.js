d5d94ce518fbb60084827ca85af5737184af4cbb
Developer-zahir <zahirulislam.iv@gmail.com>
2023-05-07 02:18:03 +0600

    note

---
+++ Day 1/main.js
@@ -0,0 +1,14 @@
+/*
+In this class we will learn ...............
+alert()
+Console.log()
+console.error()
+console.dir()
+console.table({
+    name:"zahir";
+    roll: 441916
+})
+conform()
+prompt("Enter your name")
+console.log(prompt("Enter your name"))
+*/
