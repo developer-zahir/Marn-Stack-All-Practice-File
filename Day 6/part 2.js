6d72d4717683189f4154ef1552d7823c8634247c
Developer Zahir <zahirulislam.iv@gmail.com>
2023-06-08 05:59:10 -0700

    part 3 done

--- Day 6/part 2.js
+++ Day 6/part 2.js
@@ -1,37 +1,3 @@
-// // javascript object practice
-
-// // Example of array
-// const devArray = ["Zair", 20, "IOS"];
-// console.log(devArray[0]); //output: Zair
-
-// //  Exaple of object
-// const dev = {
-//   name: "Zair",
-//   age: 20,
-//   skill: "IOS",
-// };
-// console.log(dev["name"]); // output: Zahir
-// console.log(dev.name); // output: zahir ( standard  way )
-
-// const student = {
-//   name: "Zahirul Islam",
-//   age: 20,
-//   location: "Bauphal",
-//   foods: ["haji briyani", "kaji briyani", "nanna briyani"],
-//   edu: {
-//     jsc: 5,
-//     psc: 5,
-//     ssc: 5,
-//     hsc: 5,
-//   },
-// };
-
-// console.log(
-//   `My name is ${student.name}, Age:${student.age}, I love ${student.foods.join(
-//     ","
-//   )}`
-// );
-
 const students = [
   {
     id: 1,
---
+++ Day 6/part 3.js
@@ -0,0 +1,18 @@
+const students = [
+  {
+    id: 1,
+    name: "Nusrat",
+    age: 5,
+    location: "Mirpur",
+    gender: "Female",
+    isMarried: false,
+    fev_movies:['KGF', 'Pathan', 'Pushpa'],
+    result: {
+      ban: 60,
+      eng: 80,
+      scingce: 70,
+      social_scienc: 56,
+      reli: 55,
+    },
+  },
+];
