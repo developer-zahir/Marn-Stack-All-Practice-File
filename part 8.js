7a43ce3d4824251970ace9e4490d077cf4170761
Developer Zahir <zahirulislam.iv@gmail.com>
2023-06-13 00:00:00 +0600

    hh

--- part 8.js
+++ part 8.js
@@ -1,7 +1,6 @@
 //  class topic
 // Mutable and immutable data updating
 
-
 // data updating
 // const dev = {
 //   name: "Md Zahirul Islam",
@@ -15,20 +14,30 @@
 // console.log(dev.name);
 // console.log((dev.name = "Abdul Momin"));
 
-const dev2 = [
+const dev = [
   {
     name: "Md Zahirul Islam",
-    age: 20,
     haveBrother: true,
+    age: 20,
     brother: {
       name: "Emon",
       age: 18,
     },
   },
+  {
+    name: "Imran",
+    haveBrother: true,
+    age: 30,
+    brother: {
+      name: "Ebrahim",
+      age: 20,
+    },
+  },
 ];
 
-console.log(dev2[0].name);
-console.log((dev2[0].name = "Abdul Momin"));
+// // console.log(dev[0].name);
+// // console.log((dev[0].name = "Abdul Momin"));
 
-// console.log(dev2.indexOf(dev2.find((data)=>data.age === 20)));
-console.log(dev2.find((data)=>data.haveBrother === true));
+console.log(dev.indexOf(dev.find((data) => data.age === 30))); // Find the index number of any object value age == 30
+console.log(dev.find((data) => data.age === 30).name = '40'); // Change the specified value this object
+console.log(dev);
