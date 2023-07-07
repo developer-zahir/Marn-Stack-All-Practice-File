07f1f422fee07d13d91e5a14b5d4186d9152680a
developer-zahir <zahirulislam.iv.com>
2023-07-08 01:40:33 +0600

    todo done

---
+++ Day 9/part 6 ( DOM Event 1)/app/main.js
@@ -0,0 +1,13 @@
+// const button = document.querySelector('button')
+// button.addEventListener('click', function(){
+//     alert('click done')
+// })
+
+
+
+// another way -------------- 
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+button.onclick = () => {
+    alert('click done')
+};
