12bec91d0b0fe5e3623a6446c163411a1874ee26
developer-zahir <zahirulislam.iv.com>
2023-08-04 01:57:48 +0600

    student management done

---
+++ Day 13/Student result project/src/functions.js
@@ -0,0 +1,59 @@
+// set data to the local storage
+const setData = (key, data) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+// get data from the local storage
+const getData = (key) => {
+  if (localStorage.getItem(key)) {
+    return JSON.parse(localStorage.getItem(key)).reverse();
+  } else {
+    return [];
+  }
+};
+
+// create alert
+const createAlert = (type, message) => {
+  return `
+    <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <p class="m-0">${message}</p>
+     </div>
+    `;
+};
+
+// check submite value is number or not
+const isNumber = (number) => {
+  const partran = /^[0-9]{1,}$/;
+  return partran.test(number);
+};
+
+// time ago function
+function timeAgo(timestamp) {
+  const currentTimestamp = Date.now();
+  const timeDifference = currentTimestamp - timestamp;
+
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (years >= 1) {
+    return years === 1 ? "1 year ago" : `${years} years ago`;
+  } else if (months >= 1) {
+    return months === 1 ? "1 month ago" : `${months} months ago`;
+  } else if (weeks >= 1) {
+    return weeks === 1 ? "1 week ago" : `${weeks} weeks ago`;
+  } else if (days >= 1) {
+    return days === 1 ? "1 day ago" : `${days} days ago`;
+  } else if (hours >= 1) {
+    return hours === 1 ? "1 hour ago" : `${hours} hours ago`;
+  } else if (minutes >= 1) {
+    return minutes === 1 ? "1 minute ago" : `${minutes} minutes ago`;
+  } else {
+    return seconds === 1 ? "1 second ago" : `${seconds} seconds ago`;
+  }
+}
---
+++ Day 13/Student result project/src/main.js
@@ -0,0 +1,147 @@
+const student_create_form = document.querySelector("#student_create_form");
+const photo = document.querySelector(".photo");
+const photo_preview = document.querySelector(".photo_preview");
+const msg = document.querySelector(".msg");
+const all_student_data = document.querySelector(".all-students-data");
+const single_siudent_view = document.querySelector(".student-result-data");
+
+// profile photo preview
+photo.onchange = (e) => {
+  if (photo) {
+    photo_preview.innerHTML = ` <img class="img-thumbnail mt-3 w-100" src="${e.target.value}" >`;
+  }
+};
+
+//  show data -----------------------
+const showData = () => {
+  const students = getData("students");
+  let content = "";
+  if (students.length > 0) {
+    students.map((student, index) => {
+      content += `
+          <tr class="align-middle">
+            <td>${index < 9 ? `0${index + 1}` : index + 1}</td>
+            <td><img style="width: 60px; object-fit: cover" class="rounded" src="${student.photo}" alt="${student.name}" /></td>
+            <td>${student.name}</td>
+            <td>${student.roll}</td>
+            <td>${student.reg}</td>
+            <td>${timeAgo(student.time)}</td>
+            <td><button class="btn btn-sm btn-success">Add result</button></td>
+            <td>
+            <button class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#show_single_student_modal" onclick="show_student_single_data('${
+              student.roll
+            }')">
+                <i class="fa-solid fa-eye"></i>
+            </button>
+            <button class="btn btn-sm btn-warning">
+                <i class="fa-solid fa-edit"></i>
+            </button>
+            <button class="btn btn-sm btn-danger" onclick = "deleteStudent('${student.roll}')">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+            </td>
+         </tr> 
+        `;
+    });
+  } else {
+    content = `
+        <tr>
+            <td colspan="8" class="text-center bg-danger-subtle">No data found</td>
+        </tr>
+    `;
+  }
+
+  all_student_data.innerHTML = content;
+};
+showData();
+
+// submite student data and send it to the local storage
+student_create_form.onsubmit = (e) => {
+  e.preventDefault();
+  //  get all form data
+  const from_data = new FormData(e.target);
+  const data = Object.fromEntries(from_data);
+  if (!data.name || !data.roll || !data.reg) {
+    msg.innerHTML = createAlert("danger", "All fields are required");
+  } else if (!isNumber(data.roll)) {
+    msg.innerHTML = createAlert("danger", "Roll number must be a number");
+  } else if (!isNumber(data.reg)) {
+    msg.innerHTML = createAlert("danger", "Registration number must be a number");
+  } else {
+    msg.innerHTML = createAlert("success", ` <b>${data.name}</b> is successfully created`);
+    const old_student = getData("students");
+    old_student.push({ ...data, result: null, time: Date.now() });
+    setData("students", old_student);
+    showData();
+    e.target.reset();
+    photo_preview.innerHTML = "";
+  }
+};
+
+// delete student from ls list
+const deleteStudent = (roll) => {
+  const conf = confirm("Are you sure, you want to delete this student from the ls list");
+  if (conf) {
+    const students = getData("students");
+    const update_students = students.filter((data, index) => data.roll !== roll);
+    setData("students", update_students);
+    showData();
+  } else {
+    alert("Your data is safe");
+  }
+};
+
+//   show induvisual student data
+
+const show_student_single_data = (roll) => {
+  const students = getData("students");
+  const single_student = students.find((data, index) => data.roll === roll);
+  single_siudent_view.innerHTML = `
+  <div class="student-info text-center p-3" style="background:#dddddd80;">
+  <img class="img-fluid w-100  " src="${single_student.photo}" alt="${single_student.name}" />
+  <h3 class="h3 mb-0 mt-2">${single_student.name}</h3>
+  <div class="d-flex gap-2 justify-content-center mt-0">
+    <span>Roll: ${single_student.roll}</span>
+    <span>Reg: ${single_student.reg}</span>
+  </div>
+</div>
+<div >
+  <table class="table table-striped border mt-3">
+    <thead class="fw-bold">
+      <tr>
+        <td>Subject</td>
+        <td>Mark</td>
+        <td>GPA</td>
+        <td>Gread</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Bangla</td>
+        <td>52</td>
+        <td>2.5</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>Bangla</td>
+        <td>52</td>
+        <td>2.5</td>
+        <td>B</td>
+      </tr>
+    </tbody>
+  </table>
+  <tfoot>
+    <tr>
+      <td colspan="4">
+        <button class="btn btn-primary px-5"><i class="fas fa-print"></i> Print</button>
+        <button class="btn btn-danger px-5" data-bs-dismiss="modal">Close</button>
+      </td>
+    </tr>
+  </tfoot>
+</div>
+  
+  
+  
+  `;
+
+};
