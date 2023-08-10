5655320bb30d6b045856207e4a997a384b54c331
developer-zahir <zahirulislam.iv.com>
2023-08-11 00:48:10 +0600

    id function add

--- Day 13/Student result project/src/main.js
+++ Day 13/Student result project/src/main.js
@@ -12,7 +12,7 @@ photo.onchange = (e) => {
   }
 };
 
-//  show data -----------------------
+// show data
 const showData = () => {
   const students = getData("students");
   let content = "";
@@ -58,7 +58,8 @@ showData();
 // submite student data and send it to the local storage
 student_create_form.onsubmit = (e) => {
   e.preventDefault();
-  //  get all form data
+
+  // get all form data
   const from_data = new FormData(e.target);
   const data = Object.fromEntries(from_data);
   if (!data.name || !data.roll || !data.reg) {
@@ -69,12 +70,20 @@ student_create_form.onsubmit = (e) => {
     msg.innerHTML = createAlert("danger", "Registration number must be a number");
   } else {
     const old_student = getData("students");
-    old_student.push({ ...data, result: null, time: Date.now() });
-    setData("students", old_student);
-    msg.innerHTML = createAlert("success", ` <b>${data.name}</b> is successfully created`);
-    showData();
-    e.target.reset();
-    photo_preview.innerHTML = "";
+
+    // check roll and registetion number exsist or not
+    if (old_student.some((item) => item.roll === data.roll)) {
+      msg.innerHTML = createAlert("danger", ` This Roll <b>( ${data.roll} ) </b> already exists`);
+    } else if (old_student.some((item) => item.reg === data.reg)) {
+      msg.innerHTML = createAlert("danger", ` This Reg No <b>( ${data.reg} ) </b> already exists`);
+    } else {
+      old_student.push({ ...data, result: null, time: Date.now() });
+      setData("students", old_student);
+      msg.innerHTML = createAlert("success", ` <b>${data.name}</b> is successfully created`);
+      showData();
+      e.target.reset();
+      photo_preview.innerHTML = "";
+    }
   }
 };
 
@@ -83,7 +92,7 @@ const deleteStudent = (roll) => {
   const conf = confirm("Are you sure, you want to delete this student from the ls list");
   if (conf) {
     const students = getData("students");
-    const update_students = students.filter((data, index) => data.roll !== roll);
+    const update_students = students.filter((data) => data.roll !== roll);
     setData("students", update_students);
     showData();
   } else {
@@ -91,17 +100,12 @@ const deleteStudent = (roll) => {
   }
 };
 
-
-
-
-
-
-
-//   show induvisual student data
+// show induvisual student data
 const show_student_single_data = (roll) => {
   const students = getData("students");
   const single_student = students.find((data) => data.roll == roll);
   single_student_container.innerHTML = `
+  <div class="text-end mb-2"> <button class="btn-close" data-bs-dismiss="modal"></button></div>
   <div class="student-info text-center p-3" style="background:#dddddd80;">
   <img class="img-fluid w-100  " src="${single_student.photo}" alt="${single_student.name}" />
   <h3 class="h3 mb-0 mt-2">${single_student.name}</h3>
@@ -135,18 +139,6 @@ const show_student_single_data = (roll) => {
       </tr>
     </tbody>
   </table>
-  <tfoot>
-    <tr>
-      <td colspan="4">
-        <button class="btn btn-primary px-5"><i class="fas fa-print"></i> Print</button>
-        <button class="btn btn-danger px-5" data-bs-dismiss="modal">Close</button>
-      </td>
-    </tr>
-  </tfoot>
 </div>
-  
-  
-  
   `;
-
 };
